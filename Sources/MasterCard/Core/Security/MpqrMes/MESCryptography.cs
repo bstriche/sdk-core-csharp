522b980f72694c3ad7744a065aa7bfdf6d68e274
Zhuo, Senxian <Senxian.Zhuo@mastercard.com>
2018-09-13 01:41:33 -0500

    Merge pull request #5 in API/sdk-core-charp from feature/update_fle_paths to develop
    Squashed commit of the following:
    
    commit a2458bbab8b138974fa75d3873d9f4f768d54b07
    Author: Zhuo, Senxian <Senxian.Zhuo@mastercard.com>
    Date:   Thu Sep 13 13:04:13 2018 +0800
    
        Add additional paths for MPQR MES FLE

--- Sources/MasterCard/Core/Security/MpqrMes/MESCryptography.cs
+++ Sources/MasterCard/Core/Security/MpqrMes/MESCryptography.cs
@@ -1,26 +1,26 @@
 ﻿/*
  * Copyright 2016 MasterCard International.
  *
- * Redistribution and use in source and binary forms, with or without modification, are 
+ * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this list of 
+ * Redistributions of source code must retain the above copyright notice, this list of
  * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of 
- * conditions and the following disclaimer in the documentation and/or other materials 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials
  * provided with the distribution.
- * Neither the name of the MasterCard International Incorporated nor the names of its 
- * contributors may be used to endorse or promote products derived from this software 
+ * Neither the name of the MasterCard International Incorporated nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
  * without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
  */
@@ -35,7 +35,7 @@ using MasterCard.Core.Model;
 using MasterCard.Core.Security.Fle;
 using Newtonsoft.Json;
 
-namespace MasterCard.Core.Security.MpqrMes 
+namespace MasterCard.Core.Security.MpqrMes
 {
     public class MESCryptography : FieldLevelEncryption
     {
@@ -70,7 +70,12 @@ namespace MasterCard.Core.Security.MpqrMes
                 "/mes/api/v1/merchants/.*/transactions",
                 "/paygo/api/v1/merchants/.*/transactions",
                 "/mpqr-mes/api/v1/merchants/.*/transactions",
-                "/mpqr-paygo/api/v1/merchants/.*/transactions"
+                "/mpqr-paygo/api/v1/merchants/.*/transactions",
+
+                "/mes/api/v1/transactions",
+                "/paygo/api/v1/transactions",
+                "/mpqr-mes/api/v1/transactions",
+                "/mpqr-paygo/api/v1/transactions"
             });
             tmpConfig.FieldsToEncrypt = new List<String>();
             tmpConfig.FieldsToDecrypt = new List<String>(new String[] { "items", "data.extraData" });
@@ -102,7 +107,7 @@ namespace MasterCard.Core.Security.MpqrMes
 
             foreach (String fieldToEncrypt in configuration.FieldsToEncrypt)
             {
-                if (!smartMap.ContainsKey(fieldToEncrypt)) 
+                if (!smartMap.ContainsKey(fieldToEncrypt))
                 {
                     continue;
                 }
